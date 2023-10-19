05bf3955d6f09f7273802424e4ad05295761ca09
slangdog <hello@brettmeyer.net>
2023-10-19 13:49:24 +0100

    network context

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-<<<<<<< HEAD
 import "./css/index.css";
-=======
-import "./index.css";
 import { NetworkContext } from "./useNetwork.tsx";
->>>>>>> 795c3b1248d51519096f07054f6e0d0f3ee6fd3a
 import { QueryContext } from "./useQueryClient.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
