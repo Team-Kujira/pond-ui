b04c572bb481ee4e6f426209deed2c50ef1e3aab
codehans <94654388+codehans@users.noreply.github.com>
2023-10-20 17:19:39 +0100

    more sh

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ -f "$HOME/.pond/info.json" ]; then
-    jq 'reduce inputs as $in (.; .chains *= $in.chains)' ~/.pond/info.json ./config.default.json  > ./config.json
+    jq 'reduce inputs as $in (.; .chains *= $in.chains)' "$HOME/.pond/info.json" "./config.default.json"  > "./config.json"
 else
-    cp ./config.default.json ./config.json
+    cp "./config.default.json" "./config.json"
 fi
